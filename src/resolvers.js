fc5f7ea87d4a3b262d927d3b7bd1e55233ab9f42
vipin-dubey <er.vipindubey@gmail.com>
2022-02-18 15:54:37 +0100

    Deprecated length field from Track and Module schema and added a new field as replacement

--- src/resolvers.js
+++ src/resolvers.js
@@ -44,6 +44,9 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+
+    /* Track.author and Track.modules resolvers */
+    durationInSeconds: ({ length }) => lenght,
   },
 };
 
